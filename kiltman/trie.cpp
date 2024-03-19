4f6a998d0a6ee70f139c261703411e2fc91b6c77
Murat Khairulin <murat.khairulin@gmail.com>
2024-03-20 02:17:13 +0500

    [kiltman] Handle metadata during conversion

--- kiltman/trie.cpp
+++ kiltman/trie.cpp
@@ -8,7 +8,7 @@ void SplitBy(const std::string& s, char sep, TWords& words) {
     words.clear();
     size_t start = 0;
     while (start < s.size()) {
-        while (start < s.size() && s.at(start) == sep) {
+        if (start < s.size() && s.at(start) == sep) {
             ++start;
         }
         size_t pos = start;
@@ -20,7 +20,7 @@ void SplitBy(const std::string& s, char sep, TWords& words) {
     }
 }
 
-void TrieBuilder::AddPath(const TRunes& path, uint16_t keyIndex) {
+void TrieBuilder::AddPath(const TRunes& path, TNode::TTransitionId transitionId, TNode::TKey keyIndex) {
     TNode* node = nodes_[0];
     for (TRuneValue runeValue : path) {
         TRuneId ch = GetRuneId(runeValue);
@@ -34,7 +34,7 @@ void TrieBuilder::AddPath(const TRunes& path, uint16_t keyIndex) {
     }
     ++pathCount_;
     textLength_ += path.size();
-    node->SetKeyIndex(keyIndex);
+    node->SetTransitionAndKey(transitionId, keyIndex);
 }
 
 uint16_t TrieBuilder::AddKeyRunes(const TRunes& runes) {
@@ -75,6 +75,8 @@ std::string TrieBuilder::GetKey(uint16_t index) const {
 }
 
 void TrieBuilder::PrintStats(Poco::Logger& logger) const {
+    logger.information("Rune count: %z", runeValues_.size());
+    logger.information("Transition count: %z", transitions_.size());
     logger.information("Key count: %z", keyRunesVec_.size());
     logger.information("Path count: %u", pathCount_);
     logger.information("Text length: %u", textLength_);
@@ -93,6 +95,7 @@ void TrieBuilder::PrintStats(Poco::Logger& logger) const {
 void TrieBuilder::PrintTrie(const std::string& filename) const {
     std::ofstream out(filename);
     PrintRunes(out);
+    PrintTransitions(out);
     PrintKeys(out);
     PrintNodes(out);
     out.close();
@@ -117,6 +120,17 @@ TRuneId TrieBuilder::GetRuneIdConst(TRuneValue rune) const {
     return it->second;
 }
 
+uint16_t TrieBuilder::GetTransitionId(const std::string& transition) {
+    auto it = transitionIds_.find(transition);
+    if (it != transitionIds_.end()) {
+        return it->second;
+    }
+    uint16_t id = (uint16_t) transitions_.size();
+    transitions_.push_back(transition);
+    transitionIds_[transition] = id;
+    return id;
+}
+
 TNode::TNodeId TrieBuilder::CreateNode() {
     TNode::TNodeId id = (TNode::TNodeId) nodes_.size();
     nodes_.emplace_back(new TNode());
@@ -127,6 +141,13 @@ void TrieBuilder::PrintRunes(std::ofstream& out) const {
     out << runeValues_.size() << '\n';
     for (auto rune : runeValues_) {
         out << static_cast<uint32_t>(rune) << '\n';
+    }
+}
+
+void TrieBuilder::PrintTransitions(std::ofstream& out) const {
+    out << transitions_.size() << '\n';
+    for (const auto& transition : transitions_) {
+        out << transition << '\n';
     }
 }
 
@@ -145,12 +166,30 @@ void TrieBuilder::PrintKeys(std::ofstream& out) const {
 void TrieBuilder::PrintNodes(std::ofstream& out) const {
     out << nodes_.size() << '\n';
     for (const auto& node : nodes_) {
-        out << static_cast<int>(node->keyIndex) << ' ' << node->children.size();
+        out << static_cast<int>(node->transitionId) << ' ';
+        out << static_cast<int>(node->keyIndex) << ' ';
+        out << node->children.size();
         for (const auto& [runeId, nodeId] : node->children) {
             out << ' ' << static_cast<uint32_t>(runeId) << ' ' << nodeId;
         }
         out << '\n';
     }
+}
+
+/**
+ * Concatenate parts starting from the second element of each vector.
+*/
+std::string JoinTransition(const TWords& keyParts, const TWords& formParts) {
+    std::string result = keyParts[1];
+    for (size_t i = 2; i < keyParts.size(); ++i) {
+        result.push_back(':');
+        result.append(keyParts[i]);
+    }
+    for (size_t i = 1; i < formParts.size(); ++i) {
+        result.push_back(':');
+        result.append(formParts[i]);
+    }
+    return result;
 }
 
 TrieBuilder BuildTrie(Poco::Logger* logger) {
@@ -161,6 +200,8 @@ TrieBuilder BuildTrie(Poco::Logger* logger) {
     std::string line;
 
     TWords lineParts;
+    TWords keyMetaParts;
+    TWords metaParts;
     std::vector<uint16_t> runes;
     if (logger) {
         logger->information("Reading forms from %s...", filepath);
@@ -175,11 +216,24 @@ TrieBuilder BuildTrie(Poco::Logger* logger) {
         if (lineParts.size() < 2) {
             throw std::runtime_error("Invalid line: " + line);
         }
-        StringToRunes(lineParts[0], runes);
+        SplitBy(lineParts[0], ':', keyMetaParts);
+        if (keyMetaParts.size() != 3) {
+            throw std::runtime_error("Invalid key with meta: " + lineParts[0]);
+        }
+        StringToRunes(keyMetaParts[0], runes);
         uint16_t keyIndex = builder.AddKeyRunes(runes);
-        for (size_t i = 0; i < lineParts.size(); ++i) {
-            StringToRunes(lineParts[i], runes);
-            builder.AddPath(runes, keyIndex);
+        for (size_t i = 1; i < lineParts.size(); ++i) {
+            SplitBy(lineParts[i], ':', metaParts);
+            if (metaParts.size() != 4) {
+                throw std::runtime_error("Invalid form with meta: " + lineParts[i]);
+            }
+            StringToRunes(metaParts[0], runes);
+            auto transitionId = builder.GetTransitionId(JoinTransition(keyMetaParts, metaParts));
+            if (transitionId >= TNode::kNoTransition) {
+                throw std::runtime_error("Too many transitions");
+            }
+            builder.AddPath(runes, transitionId, keyIndex);
+
         }
     }
     if (logger) {
--- kiltman/trie.h
+++ kiltman/trie.h
@@ -19,16 +19,20 @@ using TRuneId = uint8_t;
 constexpr TRuneId kNoRuneId = std::numeric_limits<TRuneId>::max();
 
 struct TNode {
+    using TTransitionId = uint8_t;
     using TKey = uint16_t;
     using TNodeId = uint32_t;
 
+    static constexpr TTransitionId kNoTransition = std::numeric_limits<TTransitionId>::max();
     static constexpr TKey kNoKey = std::numeric_limits<TKey>::max();
     static constexpr TNodeId kNoChild = std::numeric_limits<TNodeId>::max();
 
     TNode():
+        transitionId(kNoTransition),
         keyIndex(kNoKey)
     {}
 
+    TTransitionId transitionId;
     TKey keyIndex;
     std::map<TRuneId, TNodeId> children;
 
@@ -44,7 +48,8 @@ struct TNode {
         children[ch] = childId;
     }
 
-    void SetKeyIndex(TKey index) {
+    void SetTransitionAndKey(TTransitionId transition, TKey index) {
+        transitionId = transition;
         keyIndex = index;
     }
 
@@ -66,7 +71,8 @@ public:
         nodeCount_(1)
     {}
 
-    void AddPath(const TRunes& path, uint16_t keyIndex);
+    void AddPath(const TRunes& path, TNode::TTransitionId transitionId, TNode::TKey keyIndex);
+    uint16_t GetTransitionId(const std::string& transition);
     uint16_t AddKeyRunes(const TRunes& runes);
     const TNode* Traverse(const TRunes& path) const;
     const TNode* Traverse(const std::string& path) const;
@@ -79,11 +85,14 @@ private:
     TNode::TNodeId CreateNode();
 
     void PrintRunes(std::ofstream& out) const;
+    void PrintTransitions(std::ofstream& out) const;
     void PrintKeys(std::ofstream& out) const;
     void PrintNodes(std::ofstream& out) const;
 private:
     std::vector<TRuneValue> runeValues_;
     std::map<TRuneValue, TRuneId> runeIds_;
+    std::vector<std::string> transitions_;
+    std::map<std::string, uint16_t> transitionIds_;
     std::vector<TRunes> keyRunesVec_;
     std::vector<TNode*> nodes_;
     uint32_t pathCount_;
