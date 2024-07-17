c615e78bb859730df128abe49bdbaef20c8abbe2
Murat Khairulin <murat.khairulin@gmail.com>
2024-07-17 06:12:30 +0500

    [gc] Add basic /get_translation API call

---
+++ gc/app.py
@@ -0,0 +1,157 @@
+import argparse
+import logging
+from logging.config import dictConfig
+import os
+from os.path import join as pj
+import requests
+import sqlite3
+import sys
+import threading
+import uuid
+
+from flask import Flask, jsonify, redirect, request, make_response, send_file
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+DATABASE_PATH = "gc.db"
+app = Flask("gc_app")
+gc_instance = None
+
+
+class Gc(object):
+
+    def __init__(self, db_conn):
+        self.db_lock = threading.Lock()
+        self.db_conn = db_conn
+
+    def do_get_translations(self, src_lang, dst_lang, word):
+        query = """
+        SELECT
+            w1.word AS source_word,
+            w2.word AS translated_word
+        FROM
+            words w1
+        JOIN
+            translations t ON w1.word_id = t.word_id
+        JOIN
+            words w2 ON t.translated_word_id = w2.word_id
+        WHERE
+            w1.word = ?
+            AND w1.lang = ?
+            AND w2.lang = ?
+        LIMIT 100;
+        """
+
+        cursor = self.db_conn.cursor()
+        cursor.execute(query, (word, src_lang, dst_lang))
+
+        results = cursor.fetchall()
+
+        translations = [
+            {
+                "word": row["source_word"],
+                "translated_word": row["translated_word"],
+            }
+            for row in results
+        ]
+
+        return translations
+
+    def get_translation(self, src_lang, dst_lang, word):
+        with self.db_lock:
+            return self.do_get_translations(src_lang, dst_lang, word)
+
+
+def init_db_conn(db_path):
+    conn = sqlite3.connect(db_path, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    conn.execute("""
+CREATE TABLE IF NOT EXISTS words (
+    word_id INTEGER PRIMARY KEY,
+    word TEXT NOT NULL,
+    lang TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+    """.strip())
+    conn.execute("""
+CREATE INDEX IF NOT EXISTS idx_word ON words(word);
+    """.strip())
+
+    conn.execute("""
+CREATE TABLE IF NOT EXISTS translations (
+    translation_id INTEGER PRIMARY KEY,
+    word_id INTEGER NOT NULL,
+    translated_word_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+    """.strip())
+    conn.execute("""
+CREATE INDEX IF NOT EXISTS idx_translation ON translations(word_id);
+    """.strip())
+
+    logging.info("Database connection with %s established", db_path)
+    return conn
+
+
+def init_gc_app():
+    global gc_instance
+    db_conn = init_db_conn(DATABASE_PATH)
+    gc_instance = Gc(db_conn)
+    logging.info("GC app initialized")
+
+
+def valid_lang(lang):
+    return lang == "en" or lang == "kk" or lang == "ru"
+
+
+@app.route("/api/v1/test", methods=["GET"])
+def get_test():
+    return jsonify({"message": "You've reached GC!"}), 200
+
+
+@app.route("/api/v1/get_translation", methods=["GET"])
+def get_translation():
+    global gc_instance
+
+    src_lang = request.args.get("src")
+    dst_lang = request.args.get("dst")
+    word = request.args.get("w")
+
+    logging.info("Request /get_translation %s->%s: %s", src_lang, dst_lang, word)
+
+    if not valid_lang(src_lang):
+        logging.error("Invalid src")
+        return jsonify({"message": "Invalid request"}), 400
+    if not valid_lang(dst_lang):
+        logging.error("Invalid dst")
+        return jsonify({"message": "Invalid request"}), 400
+    if word is None or not (0 < len(word) < 64):
+        logging.error("Invalid word")
+        return jsonify({"message": "Invalid request"}), 400
+
+    translations = gc_instance.get_translation(src_lang, dst_lang, word)
+    return jsonify({"translations": translations}), 200
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+else:
+    init_gc_app()
