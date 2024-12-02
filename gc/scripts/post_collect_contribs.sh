d00d8ec4edfdb6c0142cc84bd76e6731aaae970c
Murat Khairulin <murat.khairulin@gmail.com>
2024-12-02 22:12:58 +0500

    [gc] Add testing scripts

---
+++ gc/scripts/post_collect_contribs.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+set -xe
+
+if [[ -z "$CRON_TOKEN" ]] ; then
+    echo "Error: variable CRON_TOKEN is unset"
+    exit 1
+fi
+
+GC_ENDPOINT=${GC_ENDPOINT:-http://127.0.0.1:5000}
+
+curl --verbose \
+    -X POST --location "${GC_ENDPOINT}/gcapi/v1/collect_contribs" \
+    -H "Content-Type: application/json" \
+    -d "{\"CRON_TOKEN\": \"${CRON_TOKEN}\"}"
