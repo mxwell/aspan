1d79e73210ea57a112bc2b116c081356715fe778
Murat Khairulin <murat.khairulin@gmail.com>
2025-03-19 05:30:49 +0500

    [gc] Add command to extract transcriptions

--- gc/scripts/asr_async.py
+++ gc/scripts/asr_async.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import os
 import requests
@@ -75,6 +76,10 @@ def asr_async(args):
     print(response.json())
 
 
+def make_operation_status_filepath(operation_id):
+    return f"operation_status_{operation_id}.json"
+
+
 def check_status(args):
     operation_id = args.operation_id
     assert len(operation_id) > 0
@@ -84,7 +89,72 @@ def check_status(args):
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    print(response.json())
+
+    filepath = make_operation_status_filepath(operation_id)
+    with open(filepath, "wt") as output:
+        output.write(response.text)
+    logging.info("operation status is stored to %s", filepath)
+
+
+def parse_timestamp(s):
+    assert s[-1] == "s"
+    dotpos = s.find(".")
+    if dotpos == -1:
+        return int(s[:-1]) * 1000
+    assert s[-5] == "."
+    return int(s[:-5]) * 1000 + int(s[-4:-1])
+
+
+def print_timestamp(t):
+    d = t // 1000
+    r = t - d * 1000
+    return f"{d}.{r:03}"
+
+
+def extract_transcription(args):
+    operation_id = args.operation_id
+    assert len(operation_id) > 0
+
+    with open(make_operation_status_filepath(operation_id)) as inputfile:
+        jsondoc = json.load(inputfile)
+    if not jsondoc["done"]:
+        logging.info("operation is not done: %s", str(jsondoc["done"]))
+        return
+
+    output_filepath = f"transcription_{operation_id}.jsonl"
+    min_dur = 1000000000
+    max_dur = 0
+    with open(output_filepath, "wt") as output:
+        prev_end = -1000000000
+        for chunk in jsondoc["response"]["chunks"]:
+            if chunk["channelTag"] != "1":
+                continue
+            startTime = 1000000000
+            endTime = 0
+            alt0 = chunk["alternatives"][0]
+            for word in alt0["words"]:
+                st = parse_timestamp(word["startTime"])
+                et = parse_timestamp(word["endTime"])
+                startTime = min(startTime, st)
+                endTime = max(endTime, et)
+            if prev_end > startTime + 10:
+                logging.error("overlap: prev end %d, cur start %d", prev_end, startTime)
+            prev_end = endTime
+            dur = endTime - startTime
+            if min_dur > dur:
+                min_dur = dur
+                logging.info("Update min_dur: %s, %s", print_timestamp(min_dur), alt0["text"])
+            if max_dur < dur:
+                max_dur = dur
+                logging.info("Update max_dur: %s, %s", print_timestamp(max_dur), alt0["text"])
+            jline = {
+                "text": alt0["text"],
+                "startTime": print_timestamp(startTime),
+                "endTime": print_timestamp(endTime),
+            }
+            output_line = json.dumps(jline, ensure_ascii=False)
+            output.write(f"{output_line}\n")
+    logging.info("Durations are from %s to %s", print_timestamp(min_dur), print_timestamp(max_dur))
 
 
 def main():
@@ -102,6 +172,10 @@ def main():
     check_status_parser = subparsers.add_parser("check_status")
     check_status_parser.add_argument("--operation-id", type=str, required=True)
     check_status_parser.set_defaults(func=check_status)
+
+    extract_transcription_parser = subparsers.add_parser("extract_transcription")
+    extract_transcription_parser.add_argument("--operation-id", type=str, required=True)
+    extract_transcription_parser.set_defaults(func=extract_transcription)
 
     args = parser.parse_args()
     args.func(args)
