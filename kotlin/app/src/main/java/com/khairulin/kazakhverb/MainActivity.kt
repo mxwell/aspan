ae1c43f74fb89c79ec5469295a78dd8893062d5f
Murat Khairulin <murat.khairulin@gmail.com>
2025-04-05 02:27:03 +0500

    [kotlin] Clean up

--- kotlin/app/src/main/java/com/khairulin/kazakhverb/MainActivity.kt
+++ kotlin/app/src/main/java/com/khairulin/kazakhverb/MainActivity.kt
@@ -1,17 +1,9 @@
 package com.khairulin.kazakhverb
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.khairulin.kazakhverb.config.SharedPreferencesManager
 import com.khairulin.kazakhverb.ui.theme.KazakhVerbTheme
 import com.khairulin.kazakhverb.verbdb.TrieLoader
@@ -29,31 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             KazakhVerbTheme {
                 AppNavigation()
-                /*
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-                 */
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KazakhVerbTheme {
-        Greeting("Android")
     }
 }
