cae4efc117fbe21b043f0364a147278eb15af584
Murat Khairulin <murat.khairulin@gmail.com>
2025-04-26 02:56:08 +0500

    [kotlin] Cover more topics

--- kotlin/api-server/src/main/kotlin/com/khairulin/kazakhverb/task/TaskGenerator.kt
+++ kotlin/api-server/src/main/kotlin/com/khairulin/kazakhverb/task/TaskGenerator.kt
@@ -19,6 +19,10 @@ class TaskGenerator {
 
     private val kTaskCount = 10
 
+    private val jatuBuilder: VerbBuilder by lazy {
+        VerbBuilder("жату")
+    }
+
     private fun buildSentenceStart(subject: String, objectWord: String): String {
         val obj = if (objectWord.isEmpty()) {
             " "
@@ -37,16 +41,33 @@ class TaskGenerator {
         return "${tense}${sentenceTypeHint}"
     }
 
-    private fun buildTaskDescription(sentenceStart: String, verb: String, forceExceptional: Boolean, sentenceType: SentenceType): String {
+    private fun buildTaskDescription(
+        tense: String,
+        sentenceStart: String,
+        verb: String,
+        forceExceptional: Boolean,
+        sentenceType: SentenceType = SentenceType.Statement,
+        auxVerb: String? = null
+    ): String {
         val sb = StringBuilder()
-        val hint = buildConjugationHint("переходное время", sentenceType)
+        val hint = buildConjugationHint(tense, sentenceType)
         sb.append("(${hint})\n")
         val noteOnException = if (forceExceptional) {
             ", глагол-исключение"
         } else {
             ""
         }
-        sb.append("`${sentenceStart}[${verb}${noteOnException}]`\n")
+        val auxVerb = if (auxVerb != null) {
+            ", вспомогательный глагол ${auxVerb}"
+        } else {
+            ""
+        }
+        val questionMark = if (sentenceType == SentenceType.Question) {
+            "?"
+        } else {
+            ""
+        }
+        sb.append("`${sentenceStart}[${verb}${noteOnException}${auxVerb}]${questionMark}`\n")
         return sb.toString()
     }
 
@@ -58,6 +79,16 @@ class TaskGenerator {
         return GetTasks(tasks)
     }
 
+    private fun getSentenceTypeByTaskId(taskId: Int): SentenceType {
+        return if (taskId <= 6) {
+            SentenceType.Statement
+        } else if (taskId <= 8) {
+            SentenceType.Negative
+        } else {
+            SentenceType.Question
+        }
+    }
+
     private fun genPresentTransitiveEasy() = genCommon {
         val verb = Top100.pickRandom()
         val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
@@ -67,10 +98,10 @@ class TaskGenerator {
         val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
 
         val description = buildTaskDescription(
+            "переходное время",
             sentenceStart,
             verb.verbDictForm,
             verb.forceExceptional,
-            SentenceType.Statement,
         )
         TaskItem(
             description,
@@ -82,19 +113,14 @@ class TaskGenerator {
         val verb = Top100.pickRandom()
         val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
         val grammarForm = usedForms.random()
-        val sentenceType = if (taskId <= 6) {
-            SentenceType.Statement
-        } else if (taskId <= 8) {
-            SentenceType.Negative
-        } else {
-            SentenceType.Question
-        }
+        val sentenceType = getSentenceTypeByTaskId(taskId)
 
         val phrasal = builder.presentTransitiveForm(grammarForm.person, grammarForm.number, sentenceType)
 
         val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
 
         val description = buildTaskDescription(
+            "переходное время",
             sentenceStart,
             verb.verbDictForm,
             verb.forceExceptional,
@@ -106,10 +132,192 @@ class TaskGenerator {
         )
     }
 
+    private fun genPresentContinuousEasy() = genCommon {
+        val verb = Top100.pickRandom()
+        val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
+        val grammarForm = usedForms.random()
+        val phrasal = builder.presentContinuousForm(
+            grammarForm.person,
+            grammarForm.number,
+            SentenceType.Statement,
+            jatuBuilder
+        )
+
+        val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
+
+        val description = buildTaskDescription(
+            "настоящее время",
+            sentenceStart,
+            verb.verbDictForm,
+            verb.forceExceptional,
+            auxVerb = "жату",
+        )
+        TaskItem(
+            description,
+            listOf("${sentenceStart}${phrasal.raw}")
+        )
+    }
+
+    private fun genPresentContinuous() = genCommon { taskId ->
+        val verb = Top100.pickRandom()
+        val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
+        val grammarForm = usedForms.random()
+        val sentenceType = getSentenceTypeByTaskId(taskId)
+
+        val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
+
+        val answers = mutableListOf<String>()
+        val phrasal = builder.presentContinuousForm(
+            grammarForm.person,
+            grammarForm.number,
+            sentenceType,
+            jatuBuilder
+        )
+        answers.add("${sentenceStart}${phrasal.raw}")
+        if (sentenceType == SentenceType.Negative) {
+            val phrasal2 = builder.presentContinuousForm(
+                grammarForm.person,
+                grammarForm.number,
+                sentenceType,
+                jatuBuilder,
+                negateAux = false
+            )
+            answers.add("${sentenceStart}${phrasal2.raw}")
+        }
+
+        val description = buildTaskDescription(
+            "настоящее время",
+            sentenceStart,
+            verb.verbDictForm,
+            verb.forceExceptional,
+            sentenceType,
+            auxVerb = "жату",
+        )
+        TaskItem(
+            description,
+            answers
+        )
+    }
+
+    fun genPastEasy() = genCommon {
+        val verb = Top100.pickRandom()
+        val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
+        val grammarForm = usedForms.random()
+        val phrasal = builder.past(
+            grammarForm.person,
+            grammarForm.number,
+            SentenceType.Statement,
+        )
+
+        val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
+
+        val description = buildTaskDescription(
+            "прошедшее время",
+            sentenceStart,
+            verb.verbDictForm,
+            verb.forceExceptional,
+        )
+        TaskItem(
+            description,
+            listOf("${sentenceStart}${phrasal.raw}")
+        )
+    }
+
+    fun genPast() = genCommon { taskId ->
+        val verb = Top100.pickRandom()
+        val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
+        val grammarForm = usedForms.random()
+        val sentenceType = getSentenceTypeByTaskId(taskId)
+
+        val phrasal = builder.past(grammarForm.person, grammarForm.number, sentenceType)
+
+        val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
+
+        val description = buildTaskDescription(
+            "прошедшее время",
+            sentenceStart,
+            verb.verbDictForm,
+            verb.forceExceptional,
+            sentenceType,
+        )
+        TaskItem(
+            description,
+            listOf("${sentenceStart}${phrasal.raw}")
+        )
+    }
+
+    fun genRemotePastEasy() = genCommon {
+        val verb = Top100.pickRandom()
+        val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
+        val grammarForm = usedForms.random()
+        val phrasal = builder.remotePast(
+            grammarForm.person,
+            grammarForm.number,
+            SentenceType.Statement,
+        )
+
+        val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
+
+        val description = buildTaskDescription(
+            "давнопрошедшее очевидное время",
+            sentenceStart,
+            verb.verbDictForm,
+            verb.forceExceptional,
+        )
+        TaskItem(
+            description,
+            listOf("${sentenceStart}${phrasal.raw}")
+        )
+    }
+
+    private fun genRemotePast() = genCommon { taskId ->
+        val verb = Top100.pickRandom()
+        val builder = VerbBuilder(verb.verbDictForm, verb.forceExceptional)
+        val grammarForm = usedForms.random()
+        val sentenceType = getSentenceTypeByTaskId(taskId)
+
+        val sentenceStart = buildSentenceStart(grammarForm.pronoun, verb.randomPreceding())
+
+        val answers = mutableListOf<String>()
+        val phrasal = builder.remotePast(
+            grammarForm.person,
+            grammarForm.number,
+            sentenceType,
+        )
+        answers.add("${sentenceStart}${phrasal.raw}")
+        if (sentenceType == SentenceType.Negative) {
+            val phrasal2 = builder.remotePast(
+                grammarForm.person,
+                grammarForm.number,
+                sentenceType,
+                negateAux = false
+            )
+            answers.add("${sentenceStart}${phrasal2.raw}")
+        }
+
+        val description = buildTaskDescription(
+            "давнопрошедшее очевидное время",
+            sentenceStart,
+            verb.verbDictForm,
+            verb.forceExceptional,
+            sentenceType,
+        )
+        TaskItem(
+            description,
+            answers
+        )
+    }
+
     fun generateTopicTasks(topic: TaskTopic): GetTasks? {
         return when (topic) {
             TaskTopic.CONJ_PRESENT_TRANSITIVE_EASY -> genPresentTransitiveEasy()
             TaskTopic.CONJ_PRESENT_TRANSITIVE -> genPresentTransitive()
+            TaskTopic.CONJ_PRESENT_CONTINUOUS_EASY -> genPresentContinuousEasy()
+            TaskTopic.CONJ_PRESENT_CONTINUOUS -> genPresentContinuous()
+            TaskTopic.CONJ_PAST_EASY -> genPastEasy()
+            TaskTopic.CONJ_PAST -> genPast()
+            TaskTopic.CONJ_REMOTE_PAST_EASY -> genRemotePastEasy()
+            TaskTopic.CONJ_REMOTE_PAST -> genRemotePast()
             else -> null
         }
     }
--- kotlin/api-server/src/main/kotlin/com/khairulin/kazakhverb/task/TaskTopic.kt
+++ kotlin/api-server/src/main/kotlin/com/khairulin/kazakhverb/task/TaskTopic.kt
@@ -6,7 +6,12 @@ import kotlinx.serialization.Serializable
 enum class TaskTopic {
     CONJ_PRESENT_TRANSITIVE_EASY,
     CONJ_PRESENT_TRANSITIVE,
+    CONJ_PRESENT_CONTINUOUS_EASY,
     CONJ_PRESENT_CONTINUOUS,
+    CONJ_PAST_EASY,
+    CONJ_PAST,
+    CONJ_REMOTE_PAST_EASY,
+    CONJ_REMOTE_PAST,
     ;
 
     companion object {
@@ -14,7 +19,12 @@ enum class TaskTopic {
             return when (value) {
                 CONJ_PRESENT_TRANSITIVE_EASY.name -> CONJ_PRESENT_TRANSITIVE_EASY
                 CONJ_PRESENT_TRANSITIVE.name -> CONJ_PRESENT_TRANSITIVE
+                CONJ_PRESENT_CONTINUOUS_EASY.name -> CONJ_PRESENT_CONTINUOUS_EASY
                 CONJ_PRESENT_CONTINUOUS.name -> CONJ_PRESENT_CONTINUOUS
+                CONJ_PAST_EASY.name -> CONJ_PAST_EASY
+                CONJ_PAST.name -> CONJ_PAST
+                CONJ_REMOTE_PAST_EASY.name -> CONJ_REMOTE_PAST_EASY
+                CONJ_REMOTE_PAST.name -> CONJ_REMOTE_PAST
                 else -> null
             }
         }
